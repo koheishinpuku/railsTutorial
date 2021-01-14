82a5ca72d00b64add7b12269079c3ff180972f52
koheishinpuku <kohei.shinpuku@unimal.jp>
2021-01-14 12:07:00 +0900

    fix/pimaconfig

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ import 'bootstrap';
 import '../stylesheets/application';
 require("@rails/activestorage").start()
 require("channels")
+window.$ = jQuery;
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
